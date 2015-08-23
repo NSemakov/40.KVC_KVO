0e37f757c6d95d836476df131159c84d6f1a4d7e
Nikita Semakov <n.semakov@yahoo.com>
2015-08-23 04:30:01 +0300

    Initial Commit

---
+++ 40. KVC_KVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  40. KVC_KVO
+//
+//  Created by Admin on 23.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 40. KVC_KVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  40. KVC_KVO
+//
+//  Created by Admin on 23.08.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
